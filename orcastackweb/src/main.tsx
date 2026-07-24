ba86c13ba0644693de202af86b28b3808c9cbd13
orca-dev-stack <orcaprojectstack@gmail.com>
2026-07-24 05:58:25 -0700

    commit 1a2b3c4d5e6f7g8h9i0j

--- orcastackweb/src/main.tsx
+++ orcastackweb/src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './styles.css';
 
+const routerBasename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
